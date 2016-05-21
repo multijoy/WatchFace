49161ed87bd04915fc74376d558d74bc05a1f96d
multijoy <rich@multijoy.co.uk>
2016-05-21 18:26:04 +0100

    Converted to exemplar

--- src/main.c
+++ src/main.c
@@ -1,30 +1,15 @@
+//I'm a test comment to test github sync!
+// and some more!
+// all very odd, this github lark
+
 #include <pebble.h>
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 
-static void main_window_load(Window *window) {
-  // Get information about the Window
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
-
-  // Create the TextLayer with specific bounds
-  s_time_layer = text_layer_create(
-      GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
-
-  // Improve the layout to be more like a watchface
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
-}
-
 static void update_time() {
   // Get a tm structure
-  time_t temp = time(NULL);
+  time_t temp = time(NULL); 
   struct tm *tick_time = localtime(&temp);
 
   // Write the current hours and minutes into a buffer
@@ -38,9 +23,27 @@ static void update_time() {
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
-
 }
 
+static void main_window_load(Window *window) {
+  // Get information about the Window
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  // Create the TextLayer with specific bounds
+  s_time_layer = text_layer_create(
+      GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+
+  // Improve the layout to be more like a watchface
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+}
 
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
@@ -49,24 +52,28 @@ static void main_window_unload(Window *window) {
 
 
 static void init() {
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  // Create main Window element and assign to pointer
   s_main_window = window_create();
-  
+
+  // Set handlers to manage the elements inside the Window
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load,
-    .unload = main_window_unload 
+    .unload = main_window_unload
   });
-  
+
+  // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
 
   // Make sure the time is displayed from the start
-update_time();
+  update_time();
 
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit() {
+  // Destroy Window
   window_destroy(s_main_window);
-  
 }
 
 int main(void) {
@@ -74,4 +81,3 @@ int main(void) {
   app_event_loop();
   deinit();
 }
-
